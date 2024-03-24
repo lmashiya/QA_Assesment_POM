402fbb0ac881b01764d1cacb20591ca982401f39
nolo <lehlohonolomashiyane@gmail.com>
2024-03-24 11:17:35 +0200

    Added Locators file.

---
+++ utils/locators.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+from selenium.webdriver.common.by import By
+
+
+class FactorialPageLocators(object):
+     FACTORIAL_ERROR_MESSAGE: Tuple = ()
+     FACTORIAL_SUCCESS_MESSAGE: Tuple = ()
+     FACTORIAL_TITLE: Tuple = ()
+     FACTORIAL_CALCULATE_BTN: Tuple = ()
+     FACTORIAL_INPUT: Tuple = ()
+     FACTORIAL_ERROR_MESSAGE: Tuple = ()
+     FACTORIAL_ERROR_MESSAGE: Tuple = ()
